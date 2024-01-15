01810787e90faefca85bdd94235bae23fb815108
Paulo Henrique Silveira <paulo.silveira@gabriel.com.br>
2024-01-15 09:08:20 -0300

    updated customer_support.service and run.sh for new version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export OPENAI_API_KEY=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
-source /home/ai/customer_support/venv/bin/activate
-source /home/ai/customer_support/.env
-python3 /home/ai/customer_support/gradio_app.py >> /home/ai/customer_support/logs/gradio_app.log 2>&1
+source /home/ai/customer_support/cs_automation_app/venv/bin/activate
+source /home/ai/customer_support/cs_automation_app/.env
+python3 /home/ai/customer_support/cs_automation_app/gradio_app.py >> /home/ai/customer_support/logs/gradio_app.log 2>&1
